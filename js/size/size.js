bf76a839e671ac09529410d077c73dd4e936fba2
Marcos <mark.metalgino@gmail.com>
2024-04-12 20:21:39 -0500

    lint: lint all the files

--- js/size/size.js
+++ js/size/size.js
@@ -1,14 +1,12 @@
-
-fetch('http://127.0.0.1:5000/size/')
-    .then(response => response.json())
-    .then(sizes => {
-        let rows = sizes.map(element => createSizeTemplate(element));
-        let table = $("#sizes tbody");
-        table.append(rows);
-    });
-
+fetch("http://127.0.0.1:5000/size/")
+  .then((response) => response.json())
+  .then((sizes) => {
+    let rows = sizes.map((element) => createSizeTemplate(element));
+    let table = $("#sizes tbody");
+    table.append(rows);
+  });
 
 function createSizeTemplate(size) {
-    let template = $("#size-item-template")[0].innerHTML;
-    return Mustache.render(template, size);
+  let template = $("#size-item-template")[0].innerHTML;
+  return Mustache.render(template, size);
 }
